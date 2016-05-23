7ac0598b17786bc2d163fa8371f9b88727fb497a
Alexandr Teplyakov <Teplyakov.in@gmail.com>
2016-05-23 19:44:10 +0300

    proba

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 
 public class Main {
     public static void main (String[]args){
-    Person person1 = new Person();
+    Person person1 = new Person("John",23);
     Person person2 = new Person();
 
-        person1.setName("John");
-        person1.setAge(31);
+        House house = new House("1street");
+        //house.setAddress("1 street");
+
+        //person1.setName("John");
+        //person1.setAge(31);
 
         person2.setName("Mike");
         person2.setAge(51);
 
+
+        house.settlePerson(person1);
+        house.settlePerson(person2);
+
         person1.descritionOfPerson();
         person2.descritionOfPerson();
+        house.descriprionOfHouse();
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -2,20 +2,58 @@
 public class Person {
     protected String name = "n/a";
     protected int age = 0;
+    protected String address = "n/a";
+
+    //konstruktor
+    public Person(){    }
+    public Person(String name,int age ){
+        this.name=name;
+        this.age = age;
+
+    }
+
 
 //method
-    public void setName(String n){
+    public void setName(String n)
+    {
         this.name = n;
     }
-    public void setAge(int a){
+        public String name()
+        {
+    return this.name;
+        }
+    public void setAge(int a)
+    {
         this.age = a;
     }
+    public int age()
+    {
+        return this.age;
+    }
+
+
+    public void setAddress(String a)
+    {
+        this.address = a;
+    }
+    public String address()
+    {
+        return this.address;
+    }
+
+
+
+
+
+
     public void descritionOfPerson(){
         System.out.println("______________________________");
         System.out.println("| My name is "+name);
         System.out.println("| I`m "+age+" yers old");
+        System.out.println("My address is  "+ address);
 
     }
+
 
 
 }
